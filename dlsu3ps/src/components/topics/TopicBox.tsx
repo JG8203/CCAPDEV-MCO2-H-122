db4972b25966560299424af3361cc0fb10b265e1
stinson122 <audrey_stinson@dlsu.edu.ph>
2024-03-08 00:21:08 +0800

    topicbox error

--- dlsu3ps/src/components/topics/TopicBox.tsx
+++ dlsu3ps/src/components/topics/TopicBox.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
 import SubtopicTable from './SubtopicTable'
 import TopicHeader from './TopicHeader'
+import { Topic } from './topics.types'
 
-export default function TopicBox() {
+export default function TopicBox({topic}:{topic: Topic}) {
   return (
     <div className="flex flex-col justify-center items-center p-5">
       <div className="overflow-x-auto flex-col">
-        <TopicHeader topicName="Top 5 Cattos"/>
-        <SubtopicTable />
+        <TopicHeader topicName={topic.name} />
+        <SubtopicTable subtopics={topic.subtopics}/>
       </div>
     </div>
+    
   )
 }
+/*
+<table>
+(stuff here such as headers etc.)
+subtopics.map((subtopic) => (
+   <SubtopicRow subtopic={subtopic} /> (td object ito)
+))
+</table>
+// definition ng subtopic row
+*/
