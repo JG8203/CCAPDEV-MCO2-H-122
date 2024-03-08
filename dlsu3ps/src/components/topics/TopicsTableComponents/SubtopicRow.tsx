ad9575c0057709547b5273630aeeb3b0d9cb8789
JG8203 <joshua_gilo@dlsu.edu.ph>
2024-03-08 22:34:51 +0800

    borked comments and posts :(

---
+++ dlsu3ps/src/components/topics/TopicsTableComponents/SubtopicRow.tsx
@@ -0,0 +1,18 @@
+import { Subtopic, Topic } from '../../../app/api/topics.types'
+import LatestPostColumn from './LatestPostColumn'
+import ThreadsColumn from '../ThreadsColumn'
+import VotesColumn from '../VotesColumn'
+import SubtopicColumn from './SubtopicColumn'
+import Link from 'next/link'
+
+export default function SubtopicRow({subtopic}: {subtopic: Subtopic}) {
+    return (
+    <tr className="border-b-2 border-olive">
+        <SubtopicColumn subtopic={subtopic} />
+        <LatestPostColumn />
+        <ThreadsColumn />
+        <VotesColumn />
+    </tr>
+  )
+}
+
---
+++ dlsu3ps/src/components/topics/TopicsTableComponents/ThreadsColumn.tsx
@@ -0,0 +1,10 @@
+
+//export default function ThreadsColumn({subtopicThreads}: {subtopicThreads: Number}) {
+//{subtopicThreads.toString()}
+export default function ThreadsColumn() {
+    return (        
+        <td className="py-4 px-6">
+            2
+        </td>
+  )
+}
