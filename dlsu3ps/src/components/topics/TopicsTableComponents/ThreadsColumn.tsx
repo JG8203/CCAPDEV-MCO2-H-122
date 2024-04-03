f838d601c689990c041b7cfca2181ce98ba153de
JG8203 <111236044+JG8203@users.noreply.github.com>
2024-04-03 23:54:18 +0800

    Delete dlsu3ps/src/components/topics directory
    deleted topics dir

--- dlsu3ps/src/components/topics/TopicsTableComponents/ThreadsColumn.tsx
+++
@@ -1,9 +0,0 @@
-export default function ThreadsColumn() {
-    return (        
-        <td className="py-4 px-6">
-            <div className="flex items-center justify-center h-full">
-                2
-            </div>
-        </td>
-  )
-}
