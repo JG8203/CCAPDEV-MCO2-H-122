b0e862d1360ed4d8231ca7d1b61728053de07e60
hihelloolleh <samantha_cabreros@dlsu.edu.ph>
2024-04-03 23:53:08 +0800

    Arranged the Dialog and DialogPost

--- dlsu3ps/src/components/Dialog.tsx
+++ dlsu3ps/src/components/Dialog.tsx
@@ -19,8 +19,8 @@ export default function Dialog({title, children, commentId, onClose} : Props){
             <div className="fixed inset-0 flex items-center justify-center z-20">
                 <dialog open className="rounded-xl backdrop-filter backdrop-blur-sm bg-opacity-50">
                     <div className="w-96 max-w-full bg-gray-200 rounded-xl flex flex-col">
-                        <div className="flex items-center justify-between px-5 pt-4 bg-yellow-400">
-                            <h1 className="text-2xl">{title}</h1>
+                        <div className="rounded-t-xl flex items-center justify-between px-5 p-4 bg-olive">
+                            <h1 className="text-2xl text-beige">{title}</h1>
                         </div>
                         <div className="p-5">
                             {children}
@@ -30,7 +30,7 @@ export default function Dialog({title, children, commentId, onClose} : Props){
                                     className="mr-2 px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 focus:outline-none focus:bg-gray-400">No
                             </button>
                             <button onClick={handleDelete}
-                                    className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600">Yes
+                                    className="px-4 py-2 bg-burnt-sienna text-white rounded-md hover:bg-red-800 focus:outline-none focus:bg-red-800">Yes
                             </button>
                         </div>
                     </div>
