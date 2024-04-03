988da05d54e169bb7319ad6638a250ab163b6138
prkrptr <125182696+prkrptr@users.noreply.github.com>
2024-04-03 20:35:23 +0800

    improved navigation with delete buttons using Modal
    added comment deleted line,

--- dlsu3ps/src/app/forum/subtopic/[subtopicId]/post/[postId]/page.tsx
+++ dlsu3ps/src/app/forum/subtopic/[subtopicId]/post/[postId]/page.tsx
@@ -10,6 +10,7 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import EditDeleteComment from "@/components/ForumPost/EditDeleteComment";
 import { TimeAgo } from "@/components/ForumPost/TimeAgo";
 import VoteComment from "@/components/ForumPost/VoteComment";
+
 async function getPost(postId: string) {
   const post = await prisma.post.findUnique({
     where: { id: postId },
@@ -43,6 +44,7 @@ export default async function Page({ params }: { params: { subtopicId: string, p
     return notFound();
   }
 
+
   async function formAction(formData: FormData) {
     "use server";
 
@@ -73,9 +75,10 @@ export default async function Page({ params }: { params: { subtopicId: string, p
 
   const profileImageUrl = fetchedPost.user?.profileImage || '';
 
+
+
   return (
     <main className="flex flex-col justify-center items-center p-9">
-
       <div className='rounded-md w-11/12'>
         <div className='px-2 py-2 flex justify-between'>
           {userObject && <Vote postId={params.postId} subtopicId={params.subtopicId} />}
@@ -95,10 +98,14 @@ export default async function Page({ params }: { params: { subtopicId: string, p
 
               </div>
             </section>
-
-            {/* I put here the CommentsSection with edit delete buttons */}
             {/* inefficient i think */}
-            {fetchedPost.comments.filter(comment => !comment.isDeleted).map(async (comment) => {
+            {/*{fetchedPost.comments.filter(comment => !comment.isDeleted).map(async (comment) => {*/}
+            {/*  const user = await prisma.user.findUnique({*/}
+            {/*    where: {*/}
+            {/*      id: comment.authorId,*/}
+            {/*    },*/}
+            {/*  });*/}
+            {fetchedPost.comments.filter(comment => comment).map(async (comment) => {
               const user = await prisma.user.findUnique({
                 where: {
                   id: comment.authorId,
@@ -106,7 +113,7 @@ export default async function Page({ params }: { params: { subtopicId: string, p
               });
               return (
                 <div key={comment.id}>
-                  <div className="border border-x-2 border-b-2 border-olive flex p-4">
+                  {!comment.isDeleted ? <div className="border border-x-2 border-b-2 border-olive flex p-4">
                     {/*<VoteComment postId={params.postId} subtopicId={params.subtopicId} commentId={comment.id}/>*/}
                     {/*Vote comment dito not working pa*/}
                     <div className="flex-row">
@@ -129,14 +136,20 @@ export default async function Page({ params }: { params: { subtopicId: string, p
 
                     {currentUser?.id === comment.authorId &&
                       <EditDeleteComment postId={params.postId} subtopicId={params.subtopicId}
-                        commentId={comment.id} />}
+                        commentId={comment.id} content={comment.content} />}
+                  </div> : <div className="border border-x-2 border-b-2 border-olive flex p-4">
+                    <div className="post-content py-6 px-6 overflow-hidden flex flex-col w-full items-center">
+                      This comment has been deleted.
+                    </div>
                   </div>
+                  }
+
                 </div>
               );
             })}
-          </article>
-        </div>
-      </div>
+          </article >
+        </div >
+      </div >
 
       {userObject && (
         <form action={formAction} className="w-6/12">
@@ -180,6 +193,6 @@ export default async function Page({ params }: { params: { subtopicId: string, p
           </div>
         </form>
       )}
-    </main>
+    </main >
   );
 }
--- dlsu3ps/src/components/SearchBar.tsx
+++ dlsu3ps/src/components/SearchBar.tsx
@@ -21,48 +21,11 @@ export default function SearchBar(){
         <>
             <form className="w-4/12 ml-5" onSubmit={handleSearch}>
                 <div className="flex">
-                    <label htmlFor="search-dropdown"
-                        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
-                    <button id="dropdown-button" data-dropdown-toggle="dropdown"
-                            className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-beige bg-olive border-beige-300 rounded-s-lg hover:bg-beige hover:text-olive focus:ring-1 focus:outline-none focus:ring-olive dark:bg-olive dark:hover:bg-olive-600 dark:focus:ring-gray-700 dark:text-beige dark:border-olive-600"
-                            type="button">All categories <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true"
-                                                            xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                            viewBox="0 0 10 6">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                            d="m1 1 4 4 4-4"/>
-                    </svg></button>
-                    <div id="dropdown"
-                        className="z-10 hidden bg-beige divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-olive-700">
-                        <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
-                            <li>
-                                <button type="button"
-                                        className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mockups
-                                </button>
-                            </li>
-                            <li>
-                                <button type="button"
-                                        className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Templates
-                                </button>
-                            </li>
-                            <li>
-                                <button type="button"
-                                        className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Design
-                                </button>
-                            </li>
-                            <li>
-                                <button type="button"
-                                        className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Logos
-                                </button>
-                            </li>
-                        </ul>
-                    </div>
                     <div className="relative w-full">
                         <input type="search" id="search-dropdown"
-                            className="block p-2.5 w-full z-20 text-sm text-olive-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-white focus:ring-olive focus:border-olive dark:bg-white dark:placeholder-olive-400 dark:text-olive dark:focus:border-olive"
+                            className="block p-2.5 w-full z-20 text-sm text-olive-900 bg-gray-50 rounded-lg border-s-gray-50 border-s-2 border border-white focus:ring-olive focus:border-olive dark:bg-white dark:placeholder-olive-400 dark:text-olive dark:focus:border-olive"
                             placeholder="Search cattos around the campus..."
                                onChange={(e) => setSearchTerm(e.target.value)}
-
-
                                required/>
                         <button type="submit" 
                                 className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-beige bg-olive rounded-e-lg border border-olive hover:bg-beige hover:text-olive focus:ring-1 focus:outline-none focus:ring-olive dark:bg-olive dark:hover:bg-olive dark:focus:ring-olive">
