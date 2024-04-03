4087687e08adfd954c07b4d69d3c38c892d72b4b
JG8203 <joshua_gilo@dlsu.edu.ph>
2024-04-03 23:46:55 +0800

    fixed stuff

---
+++ dlsu3ps/src/components/Topics/TopicsTableComponents/SubtopicColumn.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import { Subtopic, Topic } from '../../../app/api/topics.types'
+
+//export default function LatestPostColumn({subtopic}: {subtopic: Subtopic}) {
+export default function SubtopicColumn({ subtopic }: { subtopic: Subtopic }) {
+  return (
+    <td scope="row" className="p-3 font-medium text-gray-900 flex">
+      <div className="flex items-center no-underline text-gray-900">
+        <div className="text-3xl p-5">
+          🐈
+        </div>
+        <div className="flex-col">
+          <div className="font-medium text-olive hover:text-gray-800">
+            <Link href={`/forum/subtopic/${subtopic.id}`}>{subtopic.name}</Link>
+          </div>
+          <div className="font-small text-dim-gray font-normal">
+            <Link href={`/forum/subtopic/${subtopic.id}`}>{subtopic.desc}</Link>
+          </div>
+        </div>
+      </div>
+  </td>
+  )
+}
---
+++ dlsu3ps/src/components/Topics/TopicsTableComponents/ThreadsColumn.tsx
@@ -0,0 +1,9 @@
+export default function ThreadsColumn() {
+    return (        
+        <td className="py-4 px-6">
+            <div className="flex items-center justify-center h-full">
+                2
+            </div>
+        </td>
+  )
+}
